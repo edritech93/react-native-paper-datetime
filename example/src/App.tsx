bee38b8ce71c8578c6011f1db11dc00854570afe
Yudi Alviska <yudi.a@weefer.co.id>
2024-03-26 15:26:27 +0700

    override example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,117 @@
-import * as React from 'react';
+import {
+  Appbar,
+  Provider,
+  Surface,
+  ThemeProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+} from 'react-native-paper';
+import React, {useState} from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+import DropDown from 'react-native-paper-dropdown';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-paper-datetime';
-
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+function App() {
+  const [nightMode, setNightmode] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [gender, setGender] = useState<string>('');
+  const [showMultiSelectDropDown, setShowMultiSelectDropDown] = useState(false);
+  const [colors, setColors] = useState<string>('');
+  const genderList = [
+    {
+      label: 'Male',
+      value: 'male',
+    },
+    {
+      label: 'Female',
+      value: 'female',
+    },
+    {
+      label: 'Others',
+      value: 'others',
+    },
+  ];
+  const colorList = [
+    {
+      label: 'White',
+      value: 'white',
+    },
+    {
+      label: 'Red',
+      value: 'red',
+    },
+    {
+      label: 'Blue',
+      value: 'blue',
+    },
+    {
+      label: 'Green',
+      value: 'green',
+    },
+    {
+      label: 'Orange',
+      value: 'orange',
+    },
+  ];
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <Provider theme={nightMode ? MD3DarkTheme : MD3LightTheme}>
+      <ThemeProvider theme={nightMode ? MD3DarkTheme : MD3LightTheme}>
+        <StatusBar
+          backgroundColor={
+            nightMode ? MD3DarkTheme.colors.surface : MD3LightTheme.colors.primary
+          }
+          barStyle={'light-content'}
+        />
+        <Appbar.Header>
+          <Appbar.Content title="React Native Paper Dropdown" />
+          <Appbar.Action
+            icon={nightMode ? 'brightness-7' : 'brightness-3'}
+            onPress={() => setNightmode(!nightMode)}
+          />
+        </Appbar.Header>
+        <Surface style={styles.containerStyle}>
+          <SafeAreaView style={styles.safeContainerStyle}>
+            <DropDown
+              label={'Gender'}
+              mode={'outlined'}
+              visible={showDropDown}
+              showDropDown={() => setShowDropDown(true)}
+              onDismiss={() => setShowDropDown(false)}
+              value={gender}
+              setValue={setGender}
+              list={genderList}
+            />
+            <View style={styles.spacerStyle} />
+            <DropDown
+              label={'Colors'}
+              mode={'outlined'}
+              visible={showMultiSelectDropDown}
+              showDropDown={() => setShowMultiSelectDropDown(true)}
+              onDismiss={() => setShowMultiSelectDropDown(false)}
+              value={colors}
+              setValue={setColors}
+              list={colorList}
+              multiSelect
+            />
+          </SafeAreaView>
+        </Surface>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerStyle: {
     flex: 1,
-    alignItems: 'center',
+  },
+  spacerStyle: {
+    marginBottom: 15,
+  },
+  safeContainerStyle: {
+    flex: 1,
+    margin: 20,
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
 });
+
+export default App;
